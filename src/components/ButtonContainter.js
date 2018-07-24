47dd5a6e8912280cf856d8bf389a0e0c62e317f5
benche <ben.che9@gmail.com>
2018-07-24 18:25:29 -0400

    adding components

---
+++ src/components/ButtonContainter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ButtonContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default ButtonContainer;
---
+++ src/components/Minus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Minus extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Minus;
